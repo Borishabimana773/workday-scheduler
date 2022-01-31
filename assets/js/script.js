b494acc6fafda6d160cb24c9480ba74731ed81b6
Borishab <habimana40@gmail.com>
2022-01-30 19:17:35 -0500

    adding button function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,42 @@
 var scheduledTime = {};
 
-function getCalendar(time){
-    for (var i = 0; i < scheduledTime.calendar.length; i++) {
-        if (scheduledTime.calendar[i].id == time){
-            return scheduledTime.calender[i].text;
-        }
+function getCalendar(time) {
+  for (var i = 0; i < scheduledTime.calendar.length; i++) {
+    if (scheduledTime.calendar[i].id == time) {
+      return scheduledTime.calender[i].text;
     }
-    return "";
+  }
+  return "";
 }
-function createElementBlock (time,textArea) {
-    
+function createElementBlock(time, textArea) {
+  var CalendarDiv = $("<div>").attr({
+    id: "calendar-" + time,
+    class: "row time-block",
+    "data-time": time,
+  });
 
+  var colTime = $("<div>").attr("class", "col-2 col-md-1 hour text-right p-3");
+  var colTextArea = $("<textarea>").attr(
+    "class",
+    "col-8 col-md-10 description"
+  );
+  var colSaveBtn = $("<button>").attr("class", "btn col-2 col-md-1 saveBtn");
+  var button = $("<i>").attr("class", "fas fa-save");
+
+  if (textArea) {
+    colTextArea.append(textArea);
+  }
+
+  if (time < 12) {
+    colTime.text(time + "AM");
+  } else if (time === 12) {
+    colTime.text(time + "PM");
+  } else {
+    colTime.text(time - 12 + "PM");
+  }
+  colSaveBtn.append(button);
+  CalendarDiv.append(colTime, colTextArea, colSaveBtn);
+  $(".container").append(CalendarDiv);
 }
 
 function timeBlock() {
@@ -21,8 +47,13 @@ function timeBlock() {
   for (var i = 0; i < workdayHours; i++) {
     var time = i + 9;
     var textArea = getCalendar(time);
-    createElementBlock(time,textArea);
+    createElementBlock(time, textArea);
   }
+}
+
+
+function SaveCalendar() {
+  localStorage.setItem("calendar", JSON.stringify(scheduledTime));
 }
 
 function setCurrentDate() {
@@ -43,4 +74,15 @@ function load() {
   setCurrentDate();
   timeBlock();
 }
+
 load();
+$(".container").on("click", "button", function () {
+  var textarea = $(this).siblings("textarea").val().trim();
+  var id = $(this).closest("time-block").attr("data-time");
+  var calendarObject = {
+    id: id,
+    text: textarea,
+  };
+
+  SaveCalendar();
+});
